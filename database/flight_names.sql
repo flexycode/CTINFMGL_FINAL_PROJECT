117d84b180e51c9f7a5789311f9d155b86089c57
Cegeurun <cegeurunws@gmail.com>
2025-02-27 21:12:32 +0800

    💾 Updated flight_names.sql
    📝 Added a comment to make it easier for users to create the schema and its tables. Also, to document changes made.
    
    🛠️ Added USE artificial_ledger_airlines_flight_booking;  To make sure that the correct schema is used. 
    🛠️ Added more values to region ENUM to include more common regions.
    
    
    🧹 Cleaned up the syntax.

--- database/flight_names.sql
+++ database/flight_names.sql
@@ -1,9 +1,42 @@
-DROP TABLE IF EXISTS `flight_names`;
-CREATE TABLE `flight_names` (
-  `flight_name_id` INT NOT NULL AUTO_INCREMENT,
-  `flight_id` INT NOT NULL,
-  `region` ENUM('Asia', 'Europe', 'America', 'Middle East', 'Oceania') NOT NULL,
-  `name` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`flight_name_id`),
-  FOREIGN KEY (`flight_id`) REFERENCES `flights` (`flight_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*
+Run the SQL scripts in this order to initialize the schema
+users_table > flights_table > carts_table > seats_table or flight_names
+*/
+
+-- Added more common regions
+
+
+USE `artificial_ledger_airlines_flight_booking`;
+DROP TABLE IF EXISTS flight_names;
+
+CREATE TABLE IF NOT EXISTS flight_names(
+ flight_name_id INT NOT NULL,
+ region ENUM(
+        -- Africa
+        'Northern Africa', 'Sub-Saharan Africa', 
+        'East Africa', 'West Africa', 'Central Africa', 'Southern Africa',
+
+        -- Asia
+        'East Asia', 'South Asia', 'Southeast Asia', 'Central Asia', 'Western Asia (Middle East)',
+
+        -- Europe
+        'Northern Europe', 'Western Europe', 'Southern Europe', 'Eastern Europe',
+
+        -- North America
+        'United States', 'Canada', 'Mexico', 'Central America', 'Caribbean',
+
+        -- South America
+        'Brazil and Southern South America', 'Andean Region', 'The Southern Cone', 'Northern South America',
+
+        -- Oceania
+        'Australia and New Zealand', 'Melanesia', 'Micronesia', 'Polynesia',
+
+        -- Antarctica
+        'Antarctica'
+    ) NOT NULL,
+    
+name VARCHAR(150) NOT NULL,
+flight_id INT NOT NULL,
+PRIMARY KEY (flight_name_id),
+FOREIGN KEY (flight_id) REFERENCES flights(flight_id)
+);
