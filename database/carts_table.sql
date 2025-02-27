20c4667666c1e9848f386eb1123dcdf9658d61e7
Cegeurun <cegeurunws@gmail.com>
2025-02-27 21:02:26 +0800

    💾 Updated carts_table.sql
    Added a comment to make it easier for users to create the schema and its tables.
    
    Added USE `artificial_ledger_airlines_flight_booking`; 
    To make sure that the correct schema is used.
    
    Cleaned up the syntax.

--- database/carts_table.sql
+++ database/carts_table.sql
@@ -1,10 +1,17 @@
-DROP TABLE IF EXISTS `carts`;
-CREATE TABLE `carts` (
-  `cart_id` INT NOT NULL AUTO_INCREMENT,
-  `flight_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  PRIMARY KEY (`cart_id`),
-  FOREIGN KEY (`flight_id`) REFERENCES `flights` (`flight_id`),
-  FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
- hhh
+/*
+Run the SQL scripts in this order to initialize the schema
+users_table > flights_table > carts_table > seats_table or flight_names
+*/
+
+USE `artificial_ledger_airlines_flight_booking`;
+DROP TABLE IF EXISTS carts;
+
+CREATE TABLE IF NOT EXISTS carts(
+cart_id INT NOT NULL auto_increment,
+flight_id INT NOT NULL,
+user_id INT NOT NULL,
+
+PRIMARY KEY (cart_id),
+FOREIGN KEY (flight_id) REFERENCES flights(flight_id),
+FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
